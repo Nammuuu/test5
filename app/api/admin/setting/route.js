dde2d2959cfe986d032d847c6dd8bc6e0a80dfb8
Nammuuu <88241109+Nammuuu@users.noreply.github.com>
2024-12-17 10:01:40 -0800

    update 10

--- app/api/admin/setting/route.js
+++ app/api/admin/setting/route.js
@@ -23,155 +23,40 @@ export async function GET() {
   }
 }
 
-// export async function PUT(req) {
-//   try {
-//     await connectToDatabase(); // Connect to your database
 
-//     const authHeader = req.headers.get("authorization");
-//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//       return NextResponse.json({ message: "Unauthorized: Missing or invalid authorization header" }, { status: 401 });
-//     }
+export async function GET(req) {
+  try {
+    // Connect to the database
+    await connectToDatabase();
 
-//     const token = authHeader.split(" ")[1];
-//     let decodedToken;
+    // Fetch all products from the products model
+    const settings = await Settings.findOne();
 
-//     try {
-//       decodedToken = jwt.verify(token, JWT_SECRET); // Verify the JWT
-//     } catch (error) {
-//       return NextResponse.json({ message: "Invalid token" }, { status: 401 });
-//     }
+    // Disable caching by setting no-store headers
+    const headers = new Headers({
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+      'Surrogate-Control': 'no-store',
+    });
 
-//     const userId = decodedToken.userId;
+    // Return the categories as a JSON response
+    // return NextResponse.json(settings, { status: 200 });
 
-//     // Find user and check if they are admin (assuming you have a User model and roles)
-//     const user = await User.findById(userId);
-//     if (!user || user.role !== 'admin') {
-//       return NextResponse.json({ message: 'Unauthorized: User not found or not an admin' }, { status: 401 });
-//     }
+    return NextResponse.json(
+      { message: 'Products retrieved successfully', settings },
+      {
+        status: 200,
+      }
+    );
 
-//     // Get data from the request body
-   
-//     const {
-//         enableCOD,
-//         enableStripe,
-//         stripeKey,
-//         stripeSecret,
-//         enableRazorPay,
-//         enableGoogleAnalytics,
-//         googleAnalyticsKey,
-//         themeColor,
-//         shopName,
-//         companyName,
-//         address,
-//         postCode,
-//         contact,
-//         email,
-//         website,
-//         font,
-//         contrastLayout,
-//         razorPayKey, 
-//         razorPaySecret,
-
-//         enablePayPal, // PayPal fields
-//       payPalClientId,
-//       payPalSecret,
-//       } = await req.json() || {};  // Ensure you have fallback in case fields are missing
-      
-    
-//     // Find the settings document (assuming you have only one document)
-//     let settings = await Settings.findOne();
-//     if (!settings) {
-//       // Create new settings if they don't exist
-//       settings = new Settings({});
-//     }
-
-//     // Update the settings document
-//     settings.enableCOD = enableCOD;
-//     settings.enableStripe = enableStripe;
-//     settings.stripeKey = stripeKey;
-//     settings.stripeSecret = stripeSecret;
-//     settings.enableRazorPay = enableRazorPay;
-//     settings.enableGoogleAnalytics = enableGoogleAnalytics;
-//     settings.googleAnalyticsKey = googleAnalyticsKey;
-//     settings.themeColor = themeColor;
-//     settings.shopName = shopName;
-//     settings.companyName = companyName;
-//     settings.address = address;
-//     settings.postCode = postCode;
-//     settings.contact = contact;
-//     settings.email = email;
-//     settings.website = website;
-//     settings.font = font;
-//     settings.contrastLayout = contrastLayout;
-//     settings.razorPayKey = razorPayKey; // Make sure this line is executed
-//     settings.razorPaySecret = razorPaySecret;
-
-//     try {
-//         console.log("Before Save - razorPayKey:", settings.razorPayKey);  // Log before save
-//         console.log("Before Save - razorPaySecret:", settings.razorPaySecret);
-      
-//         await settings.save();  // Try saving the document
-      
-//         console.log("Settings saved successfully!");
-//       } catch (error) {
-//         console.error("Error saving settings:", error);  // Log any error that occurs during saving
-// }
-
-      
-//     // await settings.save(); // Save the updated settings
-
-//     return NextResponse.json({ message: "Settings updated successfully" }, { status: 200 });
-//   } catch (error) {
-//     console.error('Error updating settings:', error);
-//     return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
-//   }
-// }
+  } catch (error) {
+    console.error('Failed to fetch settings:', error);
+    return NextResponse.json({ message: "Failed to fetch settings" }, { status: 500 });
+  }
+}
 
 
-
-
-// POST: Save settings
-// export async function POST(req) {
-//   try {
-//     await connectToDatabase();
-
-//     const authHeader = req.headers.get("authorization");
-//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-//     }
-
-//     const token = authHeader.split(" ")[1];
-//     let decodedToken;
-//     try {
-//       decodedToken = jwt.verify(token, JWT_SECRET);
-//     } catch (error) {
-//       return NextResponse.json({ message: "Invalid token" }, { status: 401 });
-//     }
-
-//     const user = await User.findById(decodedToken.userId);
-//     if (!user || user.role !== "admin") {
-//       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-//     }
-
-//     const body = await req.json();
-//     let settings = await Settings.findOne();
-
-//     if (!settings) {
-//       settings = new Settings(body);
-//     } else {
-//       settings.set(body); // Update settings if they exist
-//     }
-
-//     await settings.save();
-//     return NextResponse.json({ message: "Settings saved successfully" }, { status: 200 });
-//   } catch (error) {
-//     console.error('Failed to save settings:', error);
-//     return NextResponse.json({ message: "Failed to save settings" }, { status: 500 });
-//   }
-// }
-
-
-// add paypal ok 
 
 
 export async function PUT(req) {
--- app/api/user/product/home/route.js
+++ app/api/user/product/home/route.js
@@ -20,22 +20,55 @@ import Product from '../../../../../models/Product';
 //   }
 
 
+
 export async function GET(req) {
   try {
+    // Connect to the database
     await connectToDatabase();
+
+    // Fetch all products from the products model
     const products = await Product.find({});
 
+    // Disable caching by setting no-store headers
+    const headers = new Headers({
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+      'Surrogate-Control': 'no-store',
+    });
+
+    // Return the categories as a JSON response
+    
     return NextResponse.json(
       { message: 'Products retrieved successfully', products },
       {
         status: 200,
-        headers: {
-          'Cache-Control': 'no-store', // Prevent caching
-        },
       }
     );
+
   } catch (error) {
     console.error('Error retrieving products:', error);
     return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+    // Handle any errors with a 500 response
   }
 }
+
+// export async function GET(req) {
+//   try {
+//     await connectToDatabase();
+//     const products = await Product.find({});
+
+//     return NextResponse.json(
+//       { message: 'Products retrieved successfully', products },
+//       {
+//         status: 200,
+//         headers: {
+//           'Cache-Control': 'no-store', // Prevent caching
+//         },
+//       }
+//     );
+//   } catch (error) {
+//     console.error('Error retrieving products:', error);
+//     return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+//   }
+// }
