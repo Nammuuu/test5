5dc3d30cf4418f2cf518e2853b91677efcb4db48
Nammuuu <88241109+Nammuuu@users.noreply.github.com>
2024-12-22 23:39:10 -0800

    erros

--- app/(userpage)/category/[id]/page.js
+++ app/(userpage)/category/[id]/page.js
@@ -105,7 +105,7 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../../../../styles/home.module.css";
 
-import ReactPaginate from 'react-paginate';
+// import ReactPaginate from 'react-paginate';
 
 const CategoryPage = () => {
   const router = useRouter();
